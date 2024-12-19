03d330c3db7d2bc802300714da09e4caef8f6a26
jarifovi <jarifovi7@gmail.com>
2024-12-19 23:29:20 +0600

    dashboard
    dasboard setup

--- Prison_Management/src/main/java/com/example/prison_management/DashBoard.java
+++ Prison_Management/src/main/java/com/example/prison_management/DashBoard.java
@@ -2,6 +2,7 @@ package com.example.prison_management;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
@@ -18,7 +19,7 @@ public class DashBoard {
     private ComboBox userTyperComboBox;
     @javafx.fxml.FXML
     public void initialize() {
-        userTyperComboBox.getItems().addAll("visitor", "Medical Officer");
+        userTyperComboBox.getItems().addAll("Visitor", "Medical Officer");
     };
     @FXML
     void forgetPassButtononclick(ActionEvent event) {
@@ -36,11 +37,32 @@ public class DashBoard {
             return;
         }
         if (password.isEmpty ()) {
-            showAlert("Validation Error", "Please enter a Valid Password")
-            return
+            showAlert("Validation Error", "Please enter a Valid Password");
+            return;
+        }
+        if (userTyperComboBoxValue == null) {
+            showAlert("Validation Error", "Please select Valid UserType");
+            return;
+        }
+        if (userId.equals ("2230907") && password.equals("1234") && userTyperComboBoxValue.equals("Medical Officer")){
+            SceneSwitcher.switchScene("Officer-Dashboard.fxml",event,"DashBoard");
+
+        } else if (userId.equals ("2230207") && password.equals("4321") && userTyperComboBoxValue.equals("Visitor")){
+            SceneSwitcher.switchScene("Visitor.fxml",event,"DashBoard");
+
+        } else{
+            showAlert("Login Failed", "Invalid User Id or Password");
+
         }
 
 
+    }
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 
 }
